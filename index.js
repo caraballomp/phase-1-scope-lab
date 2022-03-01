381de6581da530daf60eeb2b0837a5f82d709ebe
Michelle Caraballo <caraballomp@yahoo.com>
2022-02-28 20:26:12 -0800

    commit tree 4a72184651eac1aa71b1a4609e14fdfefe7f18a4

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+
+function upperCaseCustomerName(){
+ customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    globalThis.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(lala){
+bestCustomer = lala
+}
+
+const leastFavoriteCustomer = "Michelle" 
+function changeLeastFavoriteCustomer(){
+leastFavoriteCustomer = "Tom"
+}
